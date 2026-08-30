621e8f6ddd46b8a67da976473e7bcb67560e9d51
pashazlo <69246916+pashazlo@users.noreply.github.com>
2026-08-30 15:46:36 +0500

    Fix: src/main.c, CMakeLists.txt and .gitignore updates for full memory usage

--- src/main.c
+++ src/main.c
@@ -2,14 +2,14 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
-// #include "esp_spiram.h"  // TODO: Будет использовано для SD карты
+#include "esp_psram.h"
 
 void app_main(void)
 {
     printf("\n\n=== RF Tool starting ===\n");
     printf("CPU: %s\n", CONFIG_IDF_TARGET);
     printf("Free heap: %lu bytes\n", esp_get_free_heap_size());
-    // printf("PSRAM size: %d bytes\n", esp_spiram_get_size());  // TODO: Раскомментировать при добавлении SD карты
+    printf("PSRAM size: %d bytes\n", esp_psram_get_size());
 
     while (1) {
         vTaskDelay(pdMS_TO_TICKS(2000));
