249667ac0c8f7a3a7a8d257d3319f14d635aca73
pashazlo <69246916+pashazlo@users.noreply.github.com>
2026-09-01 04:05:24 +0500

    Update display.c

--- components/display/display.c
+++ components/display/display.c
@@ -32,7 +32,7 @@ static const char *TAG = "display";
 // Adjust if your panel has a memory offset (common on 240x240 modules
 // mounted on a 240x320 controller) or is mounted rotated/mirrored.
 #define DISP_GAP_X      0       // Horizontal offset (pixels)
-#define DISP_GAP_Y      0       // Vertical offset (pixels)
+#define DISP_GAP_Y      20       // Vertical offset (pixels)
 #define DISP_SWAP_XY    true
 #define DISP_MIRROR_X   true
 #define DISP_MIRROR_Y   false
