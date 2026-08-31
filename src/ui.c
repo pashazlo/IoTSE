b7ac140440455a2633c5c0e4f9ea011293d13f2e
pashazlo <69246916+pashazlo@users.noreply.github.com>
2026-09-01 03:50:38 +0500

    Update ui.c

--- src/ui.c
+++ src/ui.c
@@ -34,20 +34,10 @@ void ui_task(void *arg) {
         vTaskDelete(NULL);
         return;
     }
-    // Заливаем экран красным
-gfx_canvas_fill(&canvas, GFX_RED);
+    gfx_canvas_fill(&canvas, GFX_BLACK);
+gfx_canvas_fill_rect(&canvas, 0, 100, 320, 20, GFX_WHITE);  // y=100, высота 20px
 gfx_canvas_flush(&canvas);
-vTaskDelay(pdMS_TO_TICKS(1000));
-
-// Рисуем белую рамку по периметру
-gfx_canvas_draw_rect(&canvas, 0, 0, 319, 239, GFX_WHITE);
-gfx_canvas_flush(&canvas);
-vTaskDelay(pdMS_TO_TICKS(1000));
-
-// Диагональ
-gfx_canvas_draw_line(&canvas, 0, 0, 319, 239, GFX_GREEN);
-gfx_canvas_flush(&canvas);
-vTaskDelay(pdMS_TO_TICKS(1000));
+vTaskDelay(pdMS_TO_TICKS(2000));
 
     // 2. Рисуем статичный экран один раз
     draw_screen_1(&canvas);
