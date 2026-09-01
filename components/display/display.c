33c271ddeeb7e9d7b247a5d7cbba847285103605
pashazlo <69246916+pashazlo@users.noreply.github.com>
2026-09-01 16:02:09 +0500

    Update display.c

--- components/display/display.c
+++ components/display/display.c
@@ -87,8 +87,9 @@ esp_err_t display_init(void)
    // 3. Создаем драйвер панели ST7789
 esp_lcd_panel_dev_config_t panel_config = {
     .reset_gpio_num = DISP_RST_GPIO,
-    .rgb_ele_order = LCD_RGB_ELEMENT_ORDER_RGB,   // ← было RGB
+    .rgb_ele_order = LCD_RGB_ELEMENT_ORDER_BGR,   // ← было RGB
     .bits_per_pixel = 16,
+    .data_endian = LCD_RGB_DATA_ENDIAN_BIG,
 };
     
     ret = esp_lcd_new_panel_st7789(s_io_handle, &panel_config, &s_panel_handle);
@@ -117,7 +118,7 @@ esp_lcd_panel_dev_config_t panel_config = {
   // --- Ориентация и цвета ---
 esp_lcd_panel_swap_xy(s_panel_handle, true);
 esp_lcd_panel_mirror(s_panel_handle, true, false);
-esp_lcd_panel_invert_color(s_panel_handle, true);
+esp_lcd_panel_invert_color(s_panel_handle, false);
 
 // Критично для 170×320
 esp_lcd_panel_set_gap(s_panel_handle, 0, 35);     // ← добавил
