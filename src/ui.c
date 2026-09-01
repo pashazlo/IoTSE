9d4e541d31a958f04c85cad43d68670ca99e1555
pashazlo <69246916+pashazlo@users.noreply.github.com>
2026-09-01 17:43:29 +0500

    Add IBM VGA font header to ui.c

--- src/ui.c
+++ src/ui.c
@@ -4,6 +4,7 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
+#include "assets/ibm_vga_font.h"
 
 static const char *TAG = "UI";
 // ===== Массив битмапа из Lopaka =====
