4ecbdd57ffdfe41f79812bae871e5fa0f74f1642
pashazlo <69246916+pashazlo@users.noreply.github.com>
2026-09-01 18:12:39 +0500

    Update ui.h

--- src/ui.h
+++ src/ui.h
@@ -1,12 +1,29 @@
 #pragma once
+
 #include "display.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-// Задача интерфейса. Запускается из main.
+typedef enum {
+    UI_EVT_NONE = 0,
+    UI_EVT_UP,
+    UI_EVT_DOWN,
+    UI_EVT_SELECT,
+    UI_EVT_BACK
+} ui_event_t;
+
+// Задача UI
 void ui_task(void *arg);
+
+// Отправка события из обычных задач
+BaseType_t ui_send_event(ui_event_t evt);
+
+// Отправка события из обработчиков прерываний (ISR кнопок)
+BaseType_t ui_send_event_from_isr(ui_event_t evt, BaseType_t *hp_task_woken);
 
 #ifdef __cplusplus
 }
