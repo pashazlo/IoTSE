8cf1c6f2fe5b53775c8f6d247546fc714306acd0
pashazlo <69246916+pashazlo@users.noreply.github.com>
2026-09-05 14:22:50 +0500

    Refactor menu event handlers to use selected pointer

--- src/ui_engine/ui_controller.c
+++ src/ui_engine/ui_controller.c
@@ -9,18 +9,21 @@
 
 static const char *TAG = "UI_CONTROLLER";
 
+
 static void handle_main_menu_event(
     ui_event_t evt,
     gfx_canvas_t *canvas
 )
 {
+    uint8_t *selected = ui_focus_main_selected();
+
     switch (evt) {
 
         case UI_EVT_UP:
         case UI_EVT_DOWN:
 
-            move_focus(
-                &main_selected,
+            ui_focus_move(
+                selected,
                 MENU_COUNT,
                 evt
             );
@@ -31,8 +34,10 @@ static void handle_main_menu_event(
 
         case UI_EVT_SELECT:
 
-            if (main_menu[main_selected].callback) {
-                main_menu[main_selected].callback();
+            if (main_menu[*selected].callback) {
+
+                main_menu[*selected].callback();
+
                 ui_render(canvas);
             }
 
@@ -41,7 +46,10 @@ static void handle_main_menu_event(
         case UI_EVT_LEFT:
         case UI_EVT_RIGHT:
 
-            ESP_LOGI(TAG, "Main menu horizontal navigation");
+            ESP_LOGI(
+                TAG,
+                "Main menu horizontal navigation"
+            );
 
             break;
 
@@ -50,18 +58,21 @@ static void handle_main_menu_event(
     }
 }
 
+
 static void handle_ir_menu_event(
     ui_event_t evt,
     gfx_canvas_t *canvas
 )
 {
+    uint8_t *selected = ui_focus_ir_selected();
+
     switch (evt) {
 
         case UI_EVT_UP:
         case UI_EVT_DOWN:
 
-            move_focus(
-                &ir_selected,
+            ui_focus_move(
+                selected,
                 IR_MENU_COUNT,
                 evt
             );
@@ -72,8 +83,10 @@ static void handle_ir_menu_event(
 
         case UI_EVT_SELECT:
 
-            if (ir_selected == IR_MENU_COUNT - 1) {
+            if (*selected == IR_MENU_COUNT - 1) {
+
                 ui_screen_set(UI_SCREEN_MAIN_MENU);
+
                 ui_render(canvas);
             }
 
@@ -82,7 +95,10 @@ static void handle_ir_menu_event(
         case UI_EVT_LEFT:
         case UI_EVT_RIGHT:
 
-            ESP_LOGI(TAG, "IR horizontal navigation");
+            ESP_LOGI(
+                TAG,
+                "IR horizontal navigation"
+            );
 
             break;
 
@@ -91,18 +107,21 @@ static void handle_ir_menu_event(
     }
 }
 
+
 static void handle_wifi_menu_event(
     ui_event_t evt,
     gfx_canvas_t *canvas
 )
 {
+    uint8_t *selected = ui_focus_wifi_selected();
+
     switch (evt) {
 
         case UI_EVT_UP:
         case UI_EVT_DOWN:
 
-            move_focus(
-                &wifi_selected,
+            ui_focus_move(
+                selected,
                 WIFI_MENU_COUNT,
                 evt
             );
@@ -113,8 +132,10 @@ static void handle_wifi_menu_event(
 
         case UI_EVT_SELECT:
 
-            if (wifi_selected == WIFI_MENU_COUNT - 1) {
+            if (*selected == WIFI_MENU_COUNT - 1) {
+
                 ui_screen_set(UI_SCREEN_MAIN_MENU);
+
                 ui_render(canvas);
             }
 
@@ -123,7 +144,10 @@ static void handle_wifi_menu_event(
         case UI_EVT_LEFT:
         case UI_EVT_RIGHT:
 
-            ESP_LOGI(TAG, "Wi-Fi horizontal navigation");
+            ESP_LOGI(
+                TAG,
+                "Wi-Fi horizontal navigation"
+            );
 
             break;
 
@@ -132,18 +156,21 @@ static void handle_wifi_menu_event(
     }
 }
 
+
 static void handle_rf_menu_event(
     ui_event_t evt,
     gfx_canvas_t *canvas
 )
 {
+    uint8_t *selected = ui_focus_rf_selected();
+
     switch (evt) {
 
         case UI_EVT_UP:
         case UI_EVT_DOWN:
 
-            move_focus(
-                &rf_selected,
+            ui_focus_move(
+                selected,
                 RF_MENU_COUNT,
                 evt
             );
@@ -154,8 +181,10 @@ static void handle_rf_menu_event(
 
         case UI_EVT_SELECT:
 
-            if (rf_selected == RF_MENU_COUNT - 1) {
+            if (*selected == RF_MENU_COUNT - 1) {
+
                 ui_screen_set(UI_SCREEN_MAIN_MENU);
+
                 ui_render(canvas);
             }
 
@@ -166,18 +195,21 @@ static void handle_rf_menu_event(
     }
 }
 
+
 static void handle_nrf_menu_event(
     ui_event_t evt,
     gfx_canvas_t *canvas
 )
 {
+    uint8_t *selected = ui_focus_nrf_selected();
+
     switch (evt) {
 
         case UI_EVT_UP:
         case UI_EVT_DOWN:
 
-            move_focus(
-                &nrf_selected,
+            ui_focus_move(
+                selected,
                 NRF_MENU_COUNT,
                 evt
             );
@@ -188,8 +220,10 @@ static void handle_nrf_menu_event(
 
         case UI_EVT_SELECT:
 
-            if (nrf_selected == NRF_MENU_COUNT - 1) {
+            if (*selected == NRF_MENU_COUNT - 1) {
+
                 ui_screen_set(UI_SCREEN_MAIN_MENU);
+
                 ui_render(canvas);
             }
 
@@ -200,18 +234,21 @@ static void handle_nrf_menu_event(
     }
 }
 
+
 static void handle_bt_menu_event(
     ui_event_t evt,
     gfx_canvas_t *canvas
 )
 {
+    uint8_t *selected = ui_focus_bt_selected();
+
     switch (evt) {
 
         case UI_EVT_UP:
         case UI_EVT_DOWN:
 
-            move_focus(
-                &bt_selected,
+            ui_focus_move(
+                selected,
                 BLUETOOTH_MENU_COUNT,
                 evt
             );
@@ -222,8 +259,10 @@ static void handle_bt_menu_event(
 
         case UI_EVT_SELECT:
 
-            if (bt_selected == BLUETOOTH_MENU_COUNT - 1) {
+            if (*selected == BLUETOOTH_MENU_COUNT - 1) {
+
                 ui_screen_set(UI_SCREEN_MAIN_MENU);
+
                 ui_render(canvas);
             }
 
@@ -234,18 +273,21 @@ static void handle_bt_menu_event(
     }
 }
 
+
 static void handle_settings_menu_event(
     ui_event_t evt,
     gfx_canvas_t *canvas
 )
 {
+    uint8_t *selected = ui_focus_settings_selected();
+
     switch (evt) {
 
         case UI_EVT_UP:
         case UI_EVT_DOWN:
 
-            move_focus(
-                &settings_selected,
+            ui_focus_move(
+                selected,
                 SETTINGS_MENU_COUNT,
                 evt
             );
@@ -256,8 +298,10 @@ static void handle_settings_menu_event(
 
         case UI_EVT_SELECT:
 
-            if (settings_selected == SETTINGS_MENU_COUNT - 1) {
+            if (*selected == SETTINGS_MENU_COUNT - 1) {
+
                 ui_screen_set(UI_SCREEN_MAIN_MENU);
+
                 ui_render(canvas);
             }
 
@@ -267,6 +311,7 @@ static void handle_settings_menu_event(
             break;
     }
 }
+
 
 void ui_controller_handle_event(
     ui_event_t evt,
