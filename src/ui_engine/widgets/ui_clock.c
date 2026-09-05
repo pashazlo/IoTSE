0bdeebb44ab3ec1aae61bb4bbd6a72c7f6b40981
pashazlo <69246916+pashazlo@users.noreply.github.com>
2026-09-05 13:29:34 +0500

    Implement clock drawing functionality

--- src/ui_engine/widgets/ui_clock.c
+++ src/ui_engine/widgets/ui_clock.c
@@ -1,1 +1,36 @@
+#include "ui_clock.h"
 
+#include "display.h"
+#include "assets/ibm_vga_font.h"
+
+#include <stddef.h>
+#include <time.h>
+
+#define UI_FONT (&Px437_IBM_VGA_8x14_2x8pt7b7a)
+
+void ui_clock_draw(gfx_canvas_t *canvas)
+{
+    char time_buffer[16];
+
+    time_t now;
+    struct tm time_info;
+
+    time(&now);
+    localtime_r(&now, &time_info);
+
+    strftime(
+        time_buffer,
+        sizeof(time_buffer),
+        "%H:%M",
+        &time_info
+    );
+
+    gfx_canvas_draw_str(
+        canvas,
+        140,
+        14,
+        time_buffer,
+        UI_FONT,
+        0xFFFF
+    );
+}
