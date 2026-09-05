b5c2b405cafb6a162d74883b8891a47e05263884
pashazlo <69246916+pashazlo@users.noreply.github.com>
2026-09-05 14:31:51 +0500

    Refactor menu event handling and remove globals

--- src/ui_engine/ui_focus.c
+++ src/ui_engine/ui_focus.c
@@ -1,72 +1,340 @@
+#include "ui_controller.h"
+
+#include "ui_screen.h"
+#include "ui_menu.h"
 #include "ui_focus.h"
+#include "ui_render.h"
 
-static uint8_t main_selected = 0;
-static uint8_t ir_selected = 0;
-static uint8_t wifi_selected = 0;
-static uint8_t rf_selected = 0;
-static uint8_t nrf_selected = 0;
-static uint8_t bt_selected = 0;
-static uint8_t settings_selected = 0;
+#include "esp_log.h"
 
-void ui_focus_move(
-    uint8_t *selected,
-    uint8_t count,
-    ui_event_t event
+static const char *TAG = "UI_CONTROLLER";
+
+
+static void handle_main_menu_event(
+    ui_event_t evt,
+    gfx_canvas_t *canvas
 )
 {
-    if (selected == NULL || count == 0) {
-        return;
+    uint8_t *selected = ui_focus_main_selected();
+
+    switch (evt) {
+
+        case UI_EVT_UP:
+        case UI_EVT_DOWN:
+
+            ui_focus_move(
+                selected,
+                MENU_COUNT,
+                evt
+            );
+
+            ui_render(canvas);
+
+            break;
+
+        case UI_EVT_SELECT:
+
+            if (main_menu[*selected].callback) {
+                main_menu[*selected].callback();
+                ui_render(canvas);
+            }
+
+            break;
+
+        case UI_EVT_LEFT:
+        case UI_EVT_RIGHT:
+
+            ESP_LOGI(
+                TAG,
+                "Main menu horizontal navigation"
+            );
+
+            break;
+
+        default:
+            break;
     }
+}
 
-    if (event == UI_EVT_UP) {
 
-        if (*selected == 0) {
-            *selected = count - 1;
-        } else {
-            (*selected)--;
-        }
+static void handle_ir_menu_event(
+    ui_event_t evt,
+    gfx_canvas_t *canvas
+)
+{
+    uint8_t *selected = ui_focus_ir_selected();
+
+    switch (evt) {
+
+        case UI_EVT_UP:
+        case UI_EVT_DOWN:
+
+            ui_focus_move(
+                selected,
+                IR_MENU_COUNT,
+                evt
+            );
+
+            ui_render(canvas);
+
+            break;
+
+        case UI_EVT_SELECT:
+
+            if (*selected == IR_MENU_COUNT - 1) {
+                ui_screen_set(UI_SCREEN_MAIN_MENU);
+                ui_render(canvas);
+            }
+
+            break;
+
+        case UI_EVT_LEFT:
+        case UI_EVT_RIGHT:
+
+            ESP_LOGI(
+                TAG,
+                "IR horizontal navigation"
+            );
+
+            break;
+
+        default:
+            break;
     }
+}
 
-    else if (event == UI_EVT_DOWN) {
 
-        if (*selected >= count - 1) {
-            *selected = 0;
-        } else {
-            (*selected)++;
-        }
+static void handle_wifi_menu_event(
+    ui_event_t evt,
+    gfx_canvas_t *canvas
+)
+{
+    uint8_t *selected = ui_focus_wifi_selected();
+
+    switch (evt) {
+
+        case UI_EVT_UP:
+        case UI_EVT_DOWN:
+
+            ui_focus_move(
+                selected,
+                WIFI_MENU_COUNT,
+                evt
+            );
+
+            ui_render(canvas);
+
+            break;
+
+        case UI_EVT_SELECT:
+
+            if (*selected == WIFI_MENU_COUNT - 1) {
+                ui_screen_set(UI_SCREEN_MAIN_MENU);
+                ui_render(canvas);
+            }
+
+            break;
+
+        case UI_EVT_LEFT:
+        case UI_EVT_RIGHT:
+
+            ESP_LOGI(
+                TAG,
+                "Wi-Fi horizontal navigation"
+            );
+
+            break;
+
+        default:
+            break;
     }
 }
-uint8_t *ui_focus_main_selected(void)
+
+
+static void handle_rf_menu_event(
+    ui_event_t evt,
+    gfx_canvas_t *canvas
+)
 {
-    return &main_selected;
+    uint8_t *selected = ui_focus_rf_selected();
+
+    switch (evt) {
+
+        case UI_EVT_UP:
+        case UI_EVT_DOWN:
+
+            ui_focus_move(
+                selected,
+                RF_MENU_COUNT,
+                evt
+            );
+
+            ui_render(canvas);
+
+            break;
+
+        case UI_EVT_SELECT:
+
+            if (*selected == RF_MENU_COUNT - 1) {
+                ui_screen_set(UI_SCREEN_MAIN_MENU);
+                ui_render(canvas);
+            }
+
+            break;
+
+        default:
+            break;
+    }
 }
 
-uint8_t *ui_focus_ir_selected(void)
+
+static void handle_nrf_menu_event(
+    ui_event_t evt,
+    gfx_canvas_t *canvas
+)
 {
-    return &ir_selected;
+    uint8_t *selected = ui_focus_nrf_selected();
+
+    switch (evt) {
+
+        case UI_EVT_UP:
+        case UI_EVT_DOWN:
+
+            ui_focus_move(
+                selected,
+                NRF_MENU_COUNT,
+                evt
+            );
+
+            ui_render(canvas);
+
+            break;
+
+        case UI_EVT_SELECT:
+
+            if (*selected == NRF_MENU_COUNT - 1) {
+                ui_screen_set(UI_SCREEN_MAIN_MENU);
+                ui_render(canvas);
+            }
+
+            break;
+
+        default:
+            break;
+    }
 }
 
-uint8_t *ui_focus_wifi_selected(void)
+
+static void handle_bt_menu_event(
+    ui_event_t evt,
+    gfx_canvas_t *canvas
+)
 {
-    return &wifi_selected;
+    uint8_t *selected = ui_focus_bt_selected();
+
+    switch (evt) {
+
+        case UI_EVT_UP:
+        case UI_EVT_DOWN:
+
+            ui_focus_move(
+                selected,
+                BLUETOOTH_MENU_COUNT,
+                evt
+            );
+
+            ui_render(canvas);
+
+            break;
+
+        case UI_EVT_SELECT:
+
+            if (*selected == BLUETOOTH_MENU_COUNT - 1) {
+                ui_screen_set(UI_SCREEN_MAIN_MENU);
+                ui_render(canvas);
+            }
+
+            break;
+
+        default:
+            break;
+    }
 }
 
-uint8_t *ui_focus_rf_selected(void)
+
+static void handle_settings_menu_event(
+    ui_event_t evt,
+    gfx_canvas_t *canvas
+)
 {
-    return &rf_selected;
+    uint8_t *selected = ui_focus_settings_selected();
+
+    switch (evt) {
+
+        case UI_EVT_UP:
+        case UI_EVT_DOWN:
+
+            ui_focus_move(
+                selected,
+                SETTINGS_MENU_COUNT,
+                evt
+            );
+
+            ui_render(canvas);
+
+            break;
+
+        case UI_EVT_SELECT:
+
+            if (*selected == SETTINGS_MENU_COUNT - 1) {
+                ui_screen_set(UI_SCREEN_MAIN_MENU);
+                ui_render(canvas);
+            }
+
+            break;
+
+        default:
+            break;
+    }
 }
 
-uint8_t *ui_focus_nrf_selected(void)
-{
-    return &nrf_selected;
-}
 
-uint8_t *ui_focus_bt_selected(void)
+void ui_controller_handle_event(
+    ui_event_t evt,
+    gfx_canvas_t *canvas
+)
 {
-    return &bt_selected;
-}
+    switch (ui_screen_get()) {
 
-uint8_t *ui_focus_settings_selected(void)
-{
-    return &settings_selected;
+        case UI_SCREEN_MAIN_MENU:
+            handle_main_menu_event(evt, canvas);
+            break;
+
+        case UI_SCREEN_IR_MENU:
+            handle_ir_menu_event(evt, canvas);
+            break;
+
+        case UI_SCREEN_RF_MENU:
+            handle_rf_menu_event(evt, canvas);
+            break;
+
+        case UI_SCREEN_NRF_MENU:
+            handle_nrf_menu_event(evt, canvas);
+            break;
+
+        case UI_SCREEN_WIFI_MENU:
+            handle_wifi_menu_event(evt, canvas);
+            break;
+
+        case UI_SCREEN_BT_MENU:
+            handle_bt_menu_event(evt, canvas);
+            break;
+
+        case UI_SCREEN_SETTINGS_MENU:
+            handle_settings_menu_event(evt, canvas);
+            break;
+
+        default:
+            break;
+    }
 }
