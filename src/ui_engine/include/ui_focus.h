02006516c7a3005134b576c664d6c2acaf388eb2
pashazlo <69246916+pashazlo@users.noreply.github.com>
2026-09-05 14:19:45 +0500

    Update ui_focus.h

--- src/ui_engine/include/ui_focus.h
+++ src/ui_engine/include/ui_focus.h
@@ -11,6 +11,13 @@ void ui_focus_move(
     uint8_t count,
     ui_event_t event
 );
+uint8_t *ui_focus_main_selected(void);
+uint8_t *ui_focus_ir_selected(void);
+uint8_t *ui_focus_wifi_selected(void);
+uint8_t *ui_focus_rf_selected(void);
+uint8_t *ui_focus_nrf_selected(void);
+uint8_t *ui_focus_bt_selected(void);
+uint8_t *ui_focus_settings_selected(void);
 
 #ifdef __cplusplus
 }
