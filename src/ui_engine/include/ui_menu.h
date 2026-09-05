a2092367f3d702adc64a977d8ca67a755c36a6ce
pashazlo <69246916+pashazlo@users.noreply.github.com>
2026-09-05 14:25:43 +0500

    Refactor menu count definitions in ui_menu.h
    Removed menu count macros for various menus.

--- src/ui_engine/include/ui_menu.h
+++ src/ui_engine/include/ui_menu.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdint.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -12,21 +14,25 @@ typedef struct {
 } menu_item_t;
 
 extern const menu_item_t main_menu[];
+extern const uint8_t main_menu_count;
 
 extern const char *ir_menu[];
-extern const char *wifi_menu[];
-extern const char *rf_menu[];
-extern const char *nrf_menu[];
-extern const char *bt_menu[];
-extern const char *settings_menu[];
+extern const uint8_t ir_menu_count;
 
-#define MENU_COUNT (sizeof(main_menu) / sizeof(main_menu[0]))
-#define IR_MENU_COUNT (sizeof(ir_menu) / sizeof(ir_menu[0]))
-#define WIFI_MENU_COUNT (sizeof(wifi_menu) / sizeof(wifi_menu[0]))
-#define RF_MENU_COUNT (sizeof(rf_menu) / sizeof(rf_menu[0]))
-#define NRF_MENU_COUNT (sizeof(nrf_menu) / sizeof(nrf_menu[0]))
-#define BLUETOOTH_MENU_COUNT (sizeof(bt_menu) / sizeof(bt_menu[0]))
-#define SETTINGS_MENU_COUNT (sizeof(settings_menu) / sizeof(settings_menu[0]))
+extern const char *wifi_menu[];
+extern const uint8_t wifi_menu_count;
+
+extern const char *rf_menu[];
+extern const uint8_t rf_menu_count;
+
+extern const char *nrf_menu[];
+extern const uint8_t nrf_menu_count;
+
+extern const char *bt_menu[];
+extern const uint8_t bt_menu_count;
+
+extern const char *settings_menu[];
+extern const uint8_t settings_menu_count;
 
 #ifdef __cplusplus
 }
