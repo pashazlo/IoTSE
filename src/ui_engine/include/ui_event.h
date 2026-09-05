7c4a5c32270185098a81e3faf487c49f6e9aa342
pashazlo <69246916+pashazlo@users.noreply.github.com>
2026-09-05 13:16:15 +0500

    Add ui_event.h for UI event enumeration

--- src/ui_engine/include/ui_event.h
+++ src/ui_engine/include/ui_event.h
@@ -1,1 +1,19 @@
+// ui_event.h
+#pragma once
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum {
+    UI_EVT_NONE = 0,
+    UI_EVT_UP,
+    UI_EVT_DOWN,
+    UI_EVT_LEFT,
+    UI_EVT_RIGHT,
+    UI_EVT_SELECT,
+} ui_event_t;
+
+#ifdef __cplusplus
+}
+#endif
