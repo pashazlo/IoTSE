7c15aca17d3eaa3ddceac3528caa4e177d4d9a8f
pashazlo <69246916+pashazlo@users.noreply.github.com>
2026-09-05 13:01:45 +0500

    Implement ui_focus_move function for navigation

--- src/ui_engine/ui_focus.c
+++ src/ui_engine/ui_focus.c
@@ -1,1 +1,30 @@
+#include "ui_focus.h"
 
+void ui_focus_move(
+    uint8_t *selected,
+    uint8_t count,
+    ui_event_t event
+)
+{
+    if (selected == NULL || count == 0) {
+        return;
+    }
+
+    if (event == UI_EVT_UP) {
+
+        if (*selected == 0) {
+            *selected = count - 1;
+        } else {
+            (*selected)--;
+        }
+    }
+
+    else if (event == UI_EVT_DOWN) {
+
+        if (*selected >= count - 1) {
+            *selected = 0;
+        } else {
+            (*selected)++;
+        }
+    }
+}
