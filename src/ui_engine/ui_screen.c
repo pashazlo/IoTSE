fdba75fbe6c09117caa7727405e664416cfa97fe
pashazlo <69246916+pashazlo@users.noreply.github.com>
2026-09-05 13:18:40 +0500

    Update ui_screen.c

--- src/ui_engine/ui_screen.c
+++ src/ui_engine/ui_screen.c
@@ -1,1 +1,13 @@
+#include "ui_screen.h"
 
+static ui_screen_t current_screen = UI_SCREEN_SPLASH;
+
+void ui_screen_set(ui_screen_t screen)
+{
+    current_screen = screen;
+}
+
+ui_screen_t ui_screen_get(void)
+{
+    return current_screen;
+}
