b357558b69951db793f51e7b9fefedbcc9631f54
pashazlo <69246916+pashazlo@users.noreply.github.com>
2026-09-09 18:38:36 +0500

    Create ui_file_editor.h

---
+++ src/fm/include/ui_file_editor.h
@@ -0,0 +1,12 @@
+#ifndef UI_FILE_EDITOR_H
+#define UI_FILE_EDITOR_H
+
+#include "ui_event.h"
+#include "gfx_canvas.h"
+
+void ui_file_editor_init(void);
+void ui_file_editor_open_file(const char *filepath);
+void ui_file_editor_handle_event(ui_event_t evt);
+void ui_file_editor_draw(gfx_canvas_t *canvas);
+
+#endif // UI_FILE_EDITOR_H
