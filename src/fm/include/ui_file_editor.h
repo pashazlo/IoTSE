d67e63f92f1714ba96149636f43f282867739864
pashazlo <69246916+pashazlo@users.noreply.github.com>
2026-09-09 18:58:25 +0500

    Update ui_file_editor.h

--- src/fm/include/ui_file_editor.h
+++ src/fm/include/ui_file_editor.h
@@ -8,5 +8,7 @@ void ui_file_editor_init(void);
 void ui_file_editor_open_file(const char *filepath);
 void ui_file_editor_handle_event(ui_event_t evt);
 void ui_file_editor_draw(gfx_canvas_t *canvas);
+bool fm_text_edit_set_line(uint16_t line_index, const char *text);
+void fm_text_edit_close(void);
 
 #endif // UI_FILE_EDITOR_H
