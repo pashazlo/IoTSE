7042f7e102e4795a72356ad9ef85a41230a243ad
pashazlo <69246916+pashazlo@users.noreply.github.com>
2026-09-09 22:50:12 +0500

    Update ui_file_editor.h

--- src/fm/include/ui_file_editor.h
+++ src/fm/include/ui_file_editor.h
@@ -1,14 +1,24 @@
 #ifndef UI_FILE_EDITOR_H
 #define UI_FILE_EDITOR_H
 
+#include <stdbool.h>
+
 #include "ui_event.h"
 #include "gfx_canvas.h"
 
+
 void ui_file_editor_init(void);
-void ui_file_editor_open_file(const char *filepath);
-void ui_file_editor_handle_event(ui_event_t evt);
-void ui_file_editor_draw(gfx_canvas_t *canvas);
-bool fm_text_edit_set_line(uint16_t line_index, const char *text);
-void fm_text_edit_close(void);
+
+bool ui_file_editor_open_file(
+    const char *filepath
+);
+
+void ui_file_editor_handle_event(
+    ui_event_t evt
+);
+
+void ui_file_editor_draw(
+    gfx_canvas_t *canvas
+);
 
 #endif // UI_FILE_EDITOR_H
